359d96140a550ed557d90be86c9445d67236ea71
saurabh-ctgs <Saurabh.gautam@ctgs.in>
2025-11-27 19:01:46 +0530

    security update

--- lib/src/core/constants/app_const.dart
+++ lib/src/core/constants/app_const.dart
@@ -1,14 +1,12 @@
 import '../../features/chat/data/models/service_model.dart';
 
 class AppConst {
-  static const String geminiApiKey = 'AIzaSyBwHqhP37UeDC6uWMmR6zZL2ySxmUss68U';
   static const String geminiBaseUrl =
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
   static const String serviceBaseUrl = 'https://cityprofessionals.connivia.com';
   static const String token = 'demand_token';
   static const String searchServiceUri = '/api/v1/customer/service/search';
 
-  // static String supabaseFunctionUrl = 'https://twnvmbrenpkqdvovhmze.supabase.co/functions/v1/make-server-1707a7e2/chat';
   static String supabaseFunctionUrl = 'https://twnvmbrenpkqdvovhmze.supabase.co/functions/v1/make-server-1fe93181/chat';
 
   static String supabaseServiceRoleToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3bnZtYnJlbnBrcWR2b3ZobXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzMzOTksImV4cCI6MjA3ODUwOTM5OX0.TljwiSBw7dl0C3jYk_3KwG04YaDP7Kbx8zM-EH-j1ow';
