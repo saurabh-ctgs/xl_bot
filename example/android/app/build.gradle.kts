2a0de7f2a5c2f5467432046be68378a9b5be1c0d
saurabh-ctgs <Saurabh.gautam@ctgs.in>
2025-12-11 13:20:02 +0530

    wip

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.sales_bot_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
